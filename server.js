4775b3926f03f92e1c0e896c4ba48f78f5b07703
adnanali000 <adnan.museel123@gmail.com>
2021-06-26 16:56:37 +0500

    PORT var save in dotenv.config file and give path in server.js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
 const app = express();
+dotenv.config({path:'config.env'})
+const PORT = process.env.PORT || 8080
 
 app.get('/',(req,res)=>{
     res.send("CRUD APPLICATION");
 })
 
-app.listen(3000,()=>{
-    console.log(`server is running on http://localhost:${3000}`);
-})
+app.listen(PORT,()=>{
+    console.log(`server is running on http://localhost:${PORT}`);
+}) 
