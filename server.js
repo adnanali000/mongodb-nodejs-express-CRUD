5acb165f4c6fac9f43a7c2385eff8aa3a0884937
adnanali000 <adnan.museel123@gmail.com>
2021-06-26 17:12:50 +0500

    http server creation and routes path for project file add in server.js file

--- server.js
+++ server.js
@@ -1,14 +1,40 @@
+//require
 const express = require('express');
-const dotenv = require('dotenv');
-
 const app = express();
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
+const { urlencoded } = require('body-parser');
+
 dotenv.config({path:'config.env'})
 const PORT = process.env.PORT || 8080
 
+//log requests
+app.use(morgan('tiny'));
+
+//parse request to body parser
+app.use(bodyparser,urlencoded({extended:true}))
+
+//set view engine
+app.set("view engine","ejs") //htm;,ejs,pug
+
+//suppose if our files are save in views -> ejs -> files than we have to define path
+// app.set("views",path.resolve(__dirname,"views/ejs"));
+
+
+//load assets
+app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
+app.use('/img',express.static(path.resolve(__dirname,"assets/img")))
+app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
+
+
+//getting response
 app.get('/',(req,res)=>{
     res.send("CRUD APPLICATION");
 })
 
+//port
 app.listen(PORT,()=>{
     console.log(`server is running on http://localhost:${PORT}`);
 }) 
