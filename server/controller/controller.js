0edd964481ab76a6160fe7c1994398094076c34d
adnanali000 <adnan.museel123@gmail.com>
2021-07-05 01:26:57 +0500

    find and update api done

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -27,11 +27,30 @@ exports.create = (req,res)=>{
 
 //retrieve and return all user / retrieve and return single user
 exports.find = (req,res)=>{
-
+    Userdb.find().then(user=>{
+        res.send(user)
+    }).catch(err=>{
+        res.status(500).send({message:err.message || "Error occured while retriving user data"});
+    })
 }
 
 //update user by user id
 exports.update = (req,res)=>{
+    if(!req.body){
+        return res.status(400).send({message:"data to update can not be empty"})
+    }
+
+    const id = req.params.id;
+    Userdb.findByIdAndUpdate(id,req.body,{useFindAndModify:false})
+    .then(data=>{
+        if(!data){
+            res.status(404).send({message:`Cannot update user with ${id}.Maybe user not found`})
+        }else{
+            res.send(data)
+        }
+    }).catch(err=>{
+        res.status(500).send({message:"Error update user information"})
+    })
 
 }
 
