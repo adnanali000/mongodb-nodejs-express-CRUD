7eabc832de6c397e63c0f993a2d3e16e121650d0
adnanali000 <adnan.museel123@gmail.com>
2021-07-05 01:10:18 +0500

    create api done

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -2,6 +2,26 @@ var Userdb = require('../modal/model');
 
 //create and save new user
 exports.create = (req,res)=>{
+    //validate request
+    if(!req.body){
+        res.status(404).send({message:"content cannot be empty"});
+        return
+    }
+
+    //new user
+    const user = new Userdb({
+        name:req.body.name,
+        email:req.body.email,
+        gender:req.body.gender,
+        status:req.body.status
+    })
+
+    //save use in the database
+    user.save(user).then(data=>{
+        res.send(data)
+    }).catch(err=>{
+        res.status(500).send({message:err.message || "some error occur during create operation"});
+    });
 
 }
 
@@ -17,5 +37,5 @@ exports.update = (req,res)=>{
 
 //delete a user with specific id
 exports.delete = (req,res)=>{
-    
+
 }
