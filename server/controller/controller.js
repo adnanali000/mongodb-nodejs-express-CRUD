c47dbc8ef5e26a74b911c4210abe915abd3be70e
adnanali000 <adnan.museel123@gmail.com>
2021-07-05 00:39:36 +0500

    API created

---
+++ server/controller/controller.js
@@ -0,0 +1,21 @@
+var Userdb = require('../modal/model');
+
+//create and save new user
+exports.create = (req,res)=>{
+
+}
+
+//retrieve and return all user / retrieve and return single user
+exports.find = (req,res)=>{
+
+}
+
+//update user by user id
+exports.update = (req,res)=>{
+
+}
+
+//delete a user with specific id
+exports.delete = (req,res)=>{
+    
+}
--- server/routes/router.js
+++ server/routes/router.js
@@ -2,14 +2,23 @@ const express = require('express');
 const route = express.Router();
 const services = require('../services/render');
 
+const controller = require('../controller/controller');
+
 //getting response from index.ejs page which we render in render.js file and import here
 route.get('/', services.homeRoutes);
 
 //route to add user page
 route.get('/add-user', services.add_user);
 
-//route to update user page
+//route to update user page 
 route.get('/update-user', services.update_user);
+
+
+//API
+route.post('/api/users',controller.create);
+route.get('/api/users',controller.find);
+route.put('/api/users/:id',controller.update);
+route.delete('/api/users/:id',controller.delete);
 
 
 
