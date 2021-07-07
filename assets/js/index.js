6a231acae54def5427949c405704eda906830c05
adnanali000 <adnan.museel123@gmail.com>
2021-07-08 00:11:13 +0500

    user updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,26 @@
 
 
 $("#add-user").submit(function(event){
-    alert("data successfully submitted");
+    alert("data successfully submitted");   
+})
+
+$("#update-user").submit(function(event){
+    event.preventDefault();
+    var unindexedArray = $(this).serializeArray();
+    var data = {};
+    $.map(unindexedArray,function(n,i){
+        data[n['name']] = n['value']; 
+    })
+    console.log(data);
+
+    var request = 
+    {
+        "url":`http://localhost:3000/api/users/${data.id}`,
+        "method":"PUT",
+        "data":data
+    }
+
+    $.ajax(request).done(function(response){
+        alert("data updated")
+    });
 })
